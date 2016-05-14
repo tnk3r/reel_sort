3ce7d27d6510ae8eb109fa593e0d68e30a75b640
tink3r <abcfilm@gmail.com>
2016-05-14 21:52:35 +0300

    Create reel_sort.py
    Init Commit to github

---
+++ reel_sort.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import subprocess, os, sys
+
+try:
+        directory = sys.argv[1]
+        file_list = subprocess.check_output(["ls", str(directory)]).split("\n")
+        current = ""
+        for file in file_list:
+                if current != file[0:4]:
+                        print "="*50
+                        current = file[0:4]
+                if file != "":
+                        print "Moving "+file+" to "+file[0:4]
+                        os.system("mkdir -p "+directory+"/"+file[0:4])
+                        os.system("mv "+directory+"/"+file+" "+directory+"/"+file[0:4])
+except IndexError:
+        print "Usage: ./reel_sort 'directory'"
+        sys.exit(1)
+except StandardError as msg:
+        print str(msg)
+        sys.exit(1)
+exit()
