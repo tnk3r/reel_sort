9d1680793d293e15b2e8cb4d6bec70dd7c67d296
tnk3r <remote.syst3m@gmail.com>
2016-05-15 15:03:40 +0200

    Added §checking

--- reel_sort.py
+++ reel_sort.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
-
 import subprocess, os, sys
-
 try:
         directory = sys.argv[1]
         file_list = subprocess.check_output(["ls", str(directory)]).split("\n")
         current = ""
         for file in file_list:
-                if current != file[0:4]:
-                        print "="*50
-                        current = file[0:4]
-                if file != "":
-                        print "Moving "+file+" to "+file[0:4]
-                        os.system("mkdir -p "+directory+"/"+file[0:4])
-                        os.system("mv "+directory+"/"+file+" "+directory+"/"+file[0:4])
+                if file[0:4] == "All ":
+                    print "skipping Main ALE File"
+                else:
+                    if "ale" or "mov" in file:
+                        if current != file[0:4]:
+                                print "="*50
+                                current = file[0:4]
+                        if file != "":
+                                print "Moving "+file+" to "+file[0:4]
+                                os.system("mkdir -p "+directory+"/"+file[0:4])
+                                os.system("mv "+directory+"/"+file+" "+directory+"/"+file[0:4])
 except IndexError:
         print "Usage: ./reel_sort 'directory'"
         sys.exit(1)
