f11d1871321781ed25fb6aa4d29c8b332faadfd9
metalworker <abcfilm@gmail.com>
2017-03-19 14:51:19 -0700

    added file to sort on 3d Clip names

---
+++ reelSort3d.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import subprocess, os, sys
+try:
+    directory = sys.argv[1]
+    file_list = subprocess.check_output(["ls", str(directory)]).split("\n")
+    current = ""
+    for file in file_list:
+        if file[0:4] == "All ":
+            print "skipping Main ALE File"
+        else:
+            if ".ale" or ".mov" or ".mxf" in file:
+                if current != file[0:6]:
+                    print "="*50
+                    current = file[0:6]
+                if file != "":
+                    print "Moving "+file+" to "+file[0:6]
+                    os.system("mkdir -p "+directory+"/"+file[0:6])
+                    os.system("mv "+directory+"/"+file+" "+directory+"/"+file[0:6])
+except IndexError:
+    print "Usage: ./reel_sort 'directory'"
+    sys.exit(1)
+except StandardError as msg:
+    print str(msg)
+    sys.exit(1)
+exit()
